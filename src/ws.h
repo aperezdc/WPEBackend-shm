b7ce225d369093098bf01f53cb0df6aca325e7fa
Zan Dobersek <zdobersek@igalia.com>
2017-05-18 22:34:34 +0200

    Add WS::Instance, a singleton object under renderer host that manages all the Wayland surfaces.

---
+++ src/ws.h
@@ -0,0 +1,40 @@
+#pragma once
+
+#include <EGL/egl.h>
+#include <EGL/eglext.h>
+#include <EGL/eglmesaext.h>
+#include <glib.h>
+#include <unordered_map>
+#include <wayland-server.h>
+
+class wpe_view_backend;
+
+namespace WS {
+
+struct Surface;
+
+struct ExportableClient {
+    virtual void exportBufferResource(struct wl_resource*) = 0;
+};
+
+class Instance {
+public:
+    static Instance& singleton();
+    ~Instance();
+
+    int createClient();
+
+    void createSurface(uint32_t, Surface*);
+    void registerViewBackend(uint32_t, ExportableClient&);
+
+private:
+    Instance();
+
+    struct wl_display* m_display;
+    struct wl_global* m_compositor;
+    GSource* m_source;
+
+    std::unordered_map<uint32_t, Surface*> m_viewBackendMap;
+};
+
+} // namespace WS
